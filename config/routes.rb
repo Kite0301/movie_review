de5617a2404c2c8d43db8c3871da37987b2d28d5
Kite0301 <kaito.titech@gmail.com>
2017-06-28 10:30:49 +0900

    Add users controller and login_form page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
+  get 'login' => 'users#login_form'
+
   get 'movies' => 'movies#index'
   get 'movies/:id' => 'movies#show'
 
