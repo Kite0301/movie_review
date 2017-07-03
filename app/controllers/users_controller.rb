832a6a759ed358e44eb90479ea7e6543f30d06fb
Kite0301 <kaito.titech@gmail.com>
2017-07-03 19:13:54 +0900

    Add signup system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,4 +21,23 @@ class UsersController < ApplicationController
     flash[:notice] = "ログアウトしました"
     redirect_to("/login")
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password]
+    )
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "ユーザー登録が完了しました"
+      redirect_to('/movies')
+    else
+      render("users/new")
+    end
+  end
 end
