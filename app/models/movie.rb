6f1848c6f63c3af3fa1a8dc3efb293b944410550
Kite0301 <kaito.titech@gmail.com>
2017-07-07 19:40:28 +0900

    Display average point in movies#show

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,13 @@
 class Movie < ApplicationRecord
   has_many :reviews
+
+  def average_point
+    reviews = self.reviews
+    return if reviews.blank?
+    total_point = 0
+    reviews.each do |review|
+      total_point += review.point
+    end
+    return (total_point.to_f / reviews.count).round(1)
+  end
 end
