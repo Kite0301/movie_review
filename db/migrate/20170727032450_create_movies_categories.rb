662290a28fd14e72933b41332e1a7832263ead9d
somakihiro <aki.air@icloud.com>
2017-07-27 19:10:47 +0900

    Add movies category model

---
+++ db/migrate/20170727032450_create_movies_categories.rb
@@ -0,0 +1,10 @@
+class CreateMoviesCategories < ActiveRecord::Migration[5.0]
+  def change
+    create_table :movies_categories do |t|
+      t.integer :movie_id
+      t.integer :category_id
+
+      t.timestamps
+    end
+  end
+end
